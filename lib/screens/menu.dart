91dc3c24c6c600269ae547ba57411762f5cfb581
Venali Soni <sonivenali.vs@gmail.com>
2021-03-20 22:26:13 +0530

    Updated Menu List

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -19,26 +19,38 @@ class _MenuState extends State<Menu> {
             style: TextStyle(
                 color: Colors.white, fontSize: 24, fontWeight: FontWeight.w700),
           )),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          buildHomeButton(
-            "World",
-          ),
-
-          buildHomeButton("Technology"),
-          buildHomeButton("Science"),
-          buildHomeButton("Global Development"),
-          buildHomeButton("Life Style"),
-          buildHomeButton("Media Network"),
-          buildHomeButton("Society"),
-          buildHomeButton("Weather"),
-          buildHomeButton("Sports"),
-          buildHomeButton("TV & Radio"),
-          buildHomeButton("Guardian Professional"),
-          buildHomeButton("games"),
-          buildHomeButton("food"),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 16,left: 8,bottom: 8),
+              child: Text("The News Glory",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700),),
+            ),
+            buildHomeButton("World"),
+            buildHomeButton("Technology"),
+            buildHomeButton("Science"),
+            buildHomeButton("Global Development"),
+            buildHomeButton("Life Style"),
+            buildHomeButton("Media Network"),
+            buildHomeButton("Society"),
+            buildHomeButton("Weather"),
+            buildHomeButton("Sports"),
+            buildHomeButton("TV & Radio"),
+            buildHomeButton("Guardian Professional"),
+            buildHomeButton("Games"),
+            buildHomeButton("Art & Design"),
+            buildHomeButton("Environment"),
+            buildHomeButton("Healthcare Network"),
+            buildHomeButton("Jobs & Advice"),
+            buildHomeButton("Music"),
+            buildHomeButton("News"),
+            buildHomeButton("Extra"),
+            buildHomeButton("leeds"),
+            buildHomeButton("Info"),
+            buildHomeButton("Help"),
+          ],
+        ),
       ),
     );
   }
@@ -47,12 +59,12 @@ class _MenuState extends State<Menu> {
     String name,
   ) {
     return Padding(
-      padding: const EdgeInsets.only(top: 16, bottom: 8, left: 8),
+      padding: const EdgeInsets.only(top: 14, bottom: 8, left: 12),
       child: Row(
         children: [
           Text(
             name,
-            style: TextStyle(color: Colors.grey[800]),
+            style: TextStyle(color: Colors.grey[800],fontSize: 15),
           ),
         ],
       ),
