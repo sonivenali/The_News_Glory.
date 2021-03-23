d74e16e532b13167a29b5ed1e061d6c1984657e3
Venali Soni <sonivenali.vs@gmail.com>
2021-03-23 23:16:32 +0530

    Created News Tab List

--- lib/screens/news_tab.dart
+++ lib/screens/news_tab.dart
@@ -1,27 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:news_daily_app/models/news_tab_model.dart';
 import 'package:news_daily_app/services/news_tab_service.dart';
-class NewsTab extends StatefulWidget {
-final String category;
 
-NewsTab(this.category);
+class NewsTab extends StatefulWidget {
+  final String category;
+
+  NewsTab(this.category);
+
   @override
   _NewsTabState createState() => _NewsTabState();
 }
 
 class _NewsTabState extends State<NewsTab> {
   final service = NewsTabService();
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<NewsTabModel>(
-     future: service.getNewsTab(widget.category),
-      builder: (context, snapshot) {
-        return ListView(
-          children: [
-            Text(widget.category)
-          ],
-        );
-      }
+        future: service.getNewsTab(widget.category),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder( itemCount: snapshot.data.response.results.length,
+                itemBuilder: (context, index) => NewsCard(
+                  news: snapshot.data.response.results[index],
+                ));
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text("Something went Wrong"),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+
+}
+
+
+
+class NewsCard extends StatelessWidget {
+  final Results news;
+  NewsCard({this.news});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(news.webTitle),
     );
   }
 }
+
+
+
